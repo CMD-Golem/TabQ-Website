048345bde866ad9eea4a8d1e9ce56172d6ae6137
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2025-12-10 22:49:55 +0100

    hot reloading

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,6 @@ use axum::{
 use serde_json;
 use reqwest;
 use http;
-use zip;
 
 pub fn generic_unauthorized_error(err: &str) -> Response {
 	let body = err.to_string();
@@ -30,13 +29,6 @@ pub fn map_reqwest_error(err: reqwest::Error) -> Response {
 }
 
 pub fn map_serde_error(err: serde_json::Error) -> Response {
-	let body = err.to_string();
-
-	println!("{body}");
-	return (StatusCode::INTERNAL_SERVER_ERROR, body).into_response();
-}
-
-pub fn map_zip_error(err: zip::result::ZipError) -> Response {
 	let body = err.to_string();
 
 	println!("{body}");
--- src/workflow.rs
+++ src/workflow.rs
@@ -1,61 +1,109 @@
 use axum::{
-	body::Bytes,
-	extract::{State,DefaultBodyLimit},
-	http::StatusCode,
+	extract::State,
 	routing::post,
 	Router,
 	response::{IntoResponse, Response},
 };
-use std::{
-	env::var,
-	fs,
-	io::Cursor,
-	path::Path,
+use http::{
+	HeaderMap,
+	header::AUTHORIZATION,
+	StatusCode
 };
-use hmac::{Hmac, Mac};
-use zip::ZipArchive;
-use hex;
-use sha2::Sha256;
-use http::HeaderMap;
-// use reqwest;
+use std::{
+	env::var, f64::consts, fs, io::Cursor, path::Path, sync::Arc
+};
+use reqwest::{self, Client};
+use serde_json::{self, Value};
+use tokio_stream::StreamExt;
+use tokio::{fs::File, io::AsyncWriteExt};
 
 use crate::error;
 
+#[derive(Clone)]
+struct AppState {
+	secret: String,
+	repository: String,
+}
+
 pub async fn router() -> Router {
-	let secret = var("GITHUB_WEBHOOK_SECRET").expect("Missing GITHUB_WEBHOOK_SECRET env var");
+	let state = AppState {
+		secret: var("GITHUB_WEBHOOK_SECRET").expect("Missing GITHUB_WEBHOOK_SECRET env var"),
+		repository: var("GITHUB_REPOSITORY").expect("Missing GITHUB_REPOSITORY env var"), // https://api.github.com/repos/CMD-Golem/TabQ-Website/contents/static?ref=main
+	};
 
 	return Router::new()
 		.route("/refresh-frontend", post(refresh))
-		.layer(DefaultBodyLimit::max(0x8000000))
-		.with_state(secret);
+		.with_state(Arc::new(state));
 }
 
 // hot refresh static files
-async fn refresh(State(secret): State<String>, headers: HeaderMap, body: Bytes) -> Result<Response, Response> {
-	let signature = headers
-		.get("x-hub-signature-256")
-		.ok_or_else(|| error::generic_unauthorized_error("Signature required"))?
+async fn refresh(State(state,): State<Arc<AppState>>, headers: HeaderMap) -> Result<Response, Response> {
+	let request_bearer = headers
+		.get(AUTHORIZATION)
+		.ok_or(error::generic_unauthorized_error("Signature required"))?
 		.to_str()
-		.map_err(|e| error::map_http_error(e))?;
+		.map_err(|e| error::map_http_error(e))?
+		.strip_prefix("Bearer ")
+		.ok_or(error::generic_unauthorized_error("Expected Bearer token"))?;
 
-	let mut mac = Hmac::<Sha256>::new_from_slice(secret.as_bytes()).unwrap();
-	mac.update(&body);
-
-	if signature != hex::encode(mac.finalize().into_bytes()) {
-		// println!("Signature is correctly detected as wrong")
+	if request_bearer != state.secret {
+		// println!("Signature is correctly detected as wrong");
 		return Err(error::generic_unauthorized_error("Signature invalied"));
 	}
 
-	// extract zip
-	let reader = Cursor::new(body);
-	let frontend_dir = Path::new("/app/frontend");
+	let client = Client::new();
+	let main_dir_json = client.get(&state.repository).send().await.map_err(error::map_reqwest_error)?.text().await.map_err(error::map_reqwest_error)?;
+
+	let serde_obj: serde_json::Value = serde_json::from_str(&main_dir_json).map_err(error::map_serde_error)?;
+	let array = serde_obj.as_array().ok_or(error::generic_unauthorized_error("Malformed "))?;
+
 	let tmp_target = Path::new("/tmp/frontend");
 
-	let mut archive = ZipArchive::new(reader).map_err(|e| error::map_zip_error(e))?;
-	archive.extract(tmp_target).map_err(|e| error::map_zip_error(e))?;
+	for item in array.iter() {
+		let item_type = item["type"].as_str().unwrap_or("");
+
+		if item["type"] == "dir" {
+			let max;
+		}
+		else {
+			let response = download_file(&item, &tmp_target).await;
+		}
+	}
+
+
+
+	let frontend_dir = Path::new("/app/frontend");
+
 
 	fs::remove_dir_all(frontend_dir).map_err(|e| error::map_path_error(e))?;
 	fs::rename(tmp_target, frontend_dir).map_err(|e| error::map_path_error(e))?;
 
 	return Ok((StatusCode::OK).into_response());
+}
+
+async fn download_file(item: &Value, folder_path: &Path) -> Result<(), String> {
+	let item_name = item["name"].as_str().ok_or("Could not read name field".to_string())?;
+	let item_download = item["download_url"].as_str().ok_or("Could not read name field".to_string())?;
+	let mut dest = File::create(folder_path.join(item_name)).await.map_err(|err| format!("{err}"))?;
+
+	let client = Client::new();
+	let response = client.get(item_download).send().await.map_err(|err| format!("{err}"))?;
+
+	if !response.status().is_success() {
+		return Err("Failed to download file".to_string());
+	}
+
+	// Stream the body and write to file
+	while let Some(chunk) = response.bytes_stream().next().await {
+		let chunk = match chunk {
+			Ok(chunk) => chunk,
+			Err(e) => {
+				println!("{e}");
+				break;
+			}
+		};
+		dest.write_all(&chunk).await.map_err(|err| format!("{err}"))?;
+	}
+
+	return Ok(());
 }
